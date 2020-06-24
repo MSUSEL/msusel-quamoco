1016f744252c57c1a59ca4d03ba4a1074da63890
Isaac Griffith <isaacgriffith@gmail.com>
2020-06-24 16:18:42 -0600

    updated

--- src/migrations/20181120060107_create_test_db.sql
+++ src/migrations/20181120060107_create_test_db.sql
@@ -513,6 +513,7 @@ create table parameters
 (
     id          INTEGER NOT NULL PRIMARY KEY Autoincrement,
     name        VARCHAR,
+    varg        INTEGER,
     parent_id   INTEGER,
     parent_type VARCHAR,
     created_at  NUMERIC,
