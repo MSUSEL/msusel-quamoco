293fad471b8665fb55e83e3a14969731ea5c8ddc
Isaac Griffith <isaacgriffith@gmail.com>
2021-03-01 07:53:11 -0700

    updated

--- src/migrations/20181120060107_create_test_db.sql
+++ src/migrations/20181120060107_create_test_db.sql
@@ -121,7 +121,7 @@ create table role_bindings
 create table refs
 (
     id          INTEGER NOT NULL PRIMARY KEY Autoincrement,
-    refKey      VARCHAR UNIQUE,
+    refKey      VARCHAR,
     type        INTEGER,
     parent_id   INTEGER,
     parent_type VARCHAR,
